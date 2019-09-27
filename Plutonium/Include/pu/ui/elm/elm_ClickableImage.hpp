d81d170ca9c04d615eb57358c94659e5db1839f9
bemardev <55793274+bemardev@users.noreply.github.com>
2019-09-27 17:21:01 +0200

    Integrate Goldleaf ClickableImage to Plutonium

---
+++ Plutonium/Include/pu/ui/elm/elm_ClickableImage.hpp
@@ -0,0 +1,50 @@
+
+/*
+
+    Goldleaf - Nintendo Switch homebrew multitool, for several purposes and with several features
+
+    Copyright 2018 - 2019 Goldleaf project, developed by XorTroll
+    This project is under the terms of GPLv3 license: https://github.com/XorTroll/Goldleaf/blob/master/LICENSE
+
+*/
+
+#pragma once
+#include <functional>
+#include <pu/ui/elm/elm_Element.hpp>
+#include <chrono>
+
+namespace pu::ui::elm
+{
+    class ClickableImage : public Element
+    {
+        public:
+            ClickableImage(s32 X, s32 Y, pu::String Image);
+            PU_SMART_CTOR(ClickableImage)
+            ~ClickableImage();
+
+            s32 GetX();
+            void SetX(s32 X);
+            s32 GetY();
+            void SetY(s32 Y);
+            s32 GetWidth();
+            void SetWidth(s32 Width);
+            s32 GetHeight();
+            void SetHeight(s32 Height);
+            pu::String GetImage();
+            void SetImage(pu::String Image);
+            bool IsImageValid();
+            void SetOnClick(std::function<void()> Callback);
+            void OnRender(pu::ui::render::Renderer::Ref &Drawer, s32 X, s32 Y);
+            void OnInput(u64 Down, u64 Up, u64 Held, bool Touch);
+        protected:
+            pu::String img;
+            pu::ui::render::NativeTexture ntex;
+            s32 x;
+            s32 y;
+            s32 w;
+            s32 h;
+            std::function<void()> cb;
+            std::chrono::steady_clock::time_point touchtp;
+            bool touched;
+    };
+}
